407e281613f422b8922356e7e7396a3d92d75318
Mariavellous <mm.alcaide38@gmail.com>
2021-12-14 21:15:07 -0800

    learned flask wtf bootstrap

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 WTF_CSRF_SECRET_KEY = "elephant"
-
-app = Flask(__name__)
-app.secret_key = "elephant"
 
 
 class LoginForm(FlaskForm):
@@ -18,6 +16,17 @@ class LoginForm(FlaskForm):
   password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
   # SubmitField, represents <input type="submit". This allows checking if submit button is pressed.
   submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "elephant"
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+  return render_template('index.html')
+
 
 
 @app.route("/login", methods=['POST', 'GET'])
@@ -37,10 +46,6 @@ def login():
       return render_template('denied.html')
   return render_template('login.html', form=login_form)
 
-
-@app.route("/")
-def home():
-  return render_template('index.html')
 
 
 if __name__ == '__main__':
